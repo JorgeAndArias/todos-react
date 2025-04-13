8615543f0f9086454af284d92d11e076ce445c2f
JORGE ARIAS <jorgeariasgil@gmail.com>
2025-04-13 18:25:46 +1000

    clean up

--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -21,25 +21,3 @@ export interface TodoGroups {
   totalCompleted: number,
   todos: Todo[],
 }
-// type NumberMonth =
-//   | "01"
-//   | "02"
-//   | "03"
-//   | "04"
-//   | "05"
-//   | "06"
-//   | "07"
-//   | "08"
-//   | "09"
-//   | "10"
-//   | "11"
-//   | "12";
-
-// type Year = `${number}${number}`;
-// type DueDate = `${NumberMonth}/${Year}` | "No Due Date";
-
-// export interface SelectedGroup {
-//   name: "all" | DueDate
-//   // total: number
-//   completed: boolean
-// }
